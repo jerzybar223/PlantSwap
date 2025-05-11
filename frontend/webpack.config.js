e79d890bd6deaff1a71aae9dea0ecb75eb514417
Jerzy Baran <73556123+jerzybar223@users.noreply.github.com>
2025-05-11 19:41:34 +0200

    wywalenie drugiego node_modules

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  devServer: {
-    allowedHosts: 'all',
-    host: 'localhost',
-    port: 3000,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+    allowedHosts: 'all'
+  }
 }; 
